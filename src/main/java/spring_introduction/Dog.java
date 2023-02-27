7bc6923a87a50c6fc63d3b5c8a1d87a3668cc8c4
agorohovcom <aleksandr.gorohov@gmail.com>
2023-02-27 09:01:56 +0300

    DI

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -1,8 +1,12 @@
 package spring_introduction;
 
 public class Dog implements Pet{
+    public Dog(){
+        System.out.println("Dog bean is created");
+    }
+
     @Override
     public void say(){
-        System.out.println("Bow-Wow, motherfucker!");
+        System.out.println("Bow-Wow!");
     }
 }
